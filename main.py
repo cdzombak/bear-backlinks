5c4ddc8975ff160d8488942753d93e775dd86b03
Chris Dzombak <chris@dzombak.com>
2020-11-24 17:00:35 -0500

    don't update notes if backlinks have not changed

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from mac import copy_to_clipboard
 
 def main():
     logger = logging.getLogger('main')
-    logger.setLevel(logging.DEBUG)
+    # logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
     notes_needing_backlinks = {stub.to_note() for stub in bear.search_term('"## Backlinks"')}
     notes_needing_backlinks = {n for n in notes_needing_backlinks if not n.trashed}
     logger.info(f'{len(notes_needing_backlinks)} notes need backlink updates')
-    now = datetime.datetime.now()
 
     backlinks = {}
     for note in notes_needing_backlinks:
@@ -37,7 +37,7 @@ def main():
             stubs_sorted = sorted(backlink_stubs, key=lambda stub: stub.title.lower())
             for stub in stubs_sorted:
                 backlinks_md = f'{backlinks_md}- [[{stub.title_escaped_for_wiki_link}]]\n'
-        backlinks_md = f'{backlinks_md}\n_Backlinks calculated {now.strftime("%B %-d, %Y at %-I:%M %p")}._\n\n'
+        backlinks_md = backlinks_md + '\n'
         note_parts = note.content.split('## Backlinks\n')
         if len(note_parts) < 2:
             logger.warning(f'{note} does not have a Backlinks header; skipping it to avoid unspecified behavior')
@@ -49,26 +49,31 @@ def main():
         if len(footer_parts) < 2:
             logger.warning(f'{note} does not have a horizontal rule (---) after its backlinks header; skipping it to avoid clobbering the rest of the note')
             continue
+        old_backlinks = footer_parts[0]
         post_backlinks = '---' + '---'.join(footer_parts[1:])
-        new_note_content[note.id] = pre_backlinks + backlinks_md + post_backlinks
+        if old_backlinks == backlinks_md:
+            logger.debug(f'note {note} does not have updated backlinks; skipping it')
+            continue
+        new_note_content[note] = pre_backlinks + backlinks_md + post_backlinks
 
-    backup_dir = os.path.join(get_backups_path(), now.strftime('%Y-%m-%dT%H-%M-%S%z'))
+    backup_dir = os.path.join(get_backups_path(), datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S%z'))
     os.mkdir(backup_dir)
-    logger.info(f'backing up {len(notes_needing_backlinks)} notes to {backup_dir}')
-    for note in notes_needing_backlinks:
+    logger.info(f'backing up {len(new_note_content)} notes to {backup_dir}')
+    for note in new_note_content.keys():
         fname = os.path.join(backup_dir, note.id+'.md')
         with open(fname, 'w') as f:
             f.write(note.content)
 
-    for note_id, new_content in new_note_content.items():
-        bear.open_note_for_edit(note_id)
-        logger.debug(f'updating content for note {note_id}')
+    for note, new_content in new_note_content.items():
+        bear.open_note_for_edit(note.id)
+        logger.debug(f'updating content for note {note.id}')
         copy_to_clipboard(new_content)
         time.sleep(0.5)
-        _ = os.popen("""osascript -e 'tell application "Bear" to activate' -e 'delay 1' -e 'tell application "System Events" to keystroke "a" using command down' -e 'delay 0.5' -e 'tell application "System Events" to keystroke "v" using command down'""").read()
+        os.popen("""osascript -e 'tell application "Bear" to activate' -e 'delay 1' -e 'tell application "System Events" to keystroke "a" using command down' -e 'delay 0.5' -e 'tell application "System Events" to keystroke "v" using command down'""").read()
         time.sleep(0.5)
 
     logger.info(f'completed successfully; updated {len(new_note_content)} notes.')
+    os.popen(f'terminal-notifier -message "✅ Updated Bear backlinks for {len(new_note_content)} notes." -title "Bear Backlinks" -sender net.shinyfrog.bear').read()
 
 
 if __name__ == "__main__":
